5a217c4eb8f8fb22cffc10fc0e90b0a84e640999
xmalachi <angelmalachi@gmail.com>
2023-02-24 09:40:33 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useEffect, useState } from 'react'
+import React, {useEffect, useState } from 'react'
 
 import Intro from './Components/Intro'
 import Portfolio from './Components/Portfolio'
